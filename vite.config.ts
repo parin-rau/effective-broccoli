7b0448ee6e5013aed896412f26694a838673370f
gaohaos <parin.rau@gmail.com>
2024-06-19 21:14:32 -0700

    fix ErrorMessage.tsx filename typo causing build failure

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 installGlobals();
 
 export default defineConfig({
 	plugins: [remix(), tsconfigPaths()],
-	resolve: { alias: { "~": path.resolve(__dirname, "./app") } },
 });
